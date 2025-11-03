064d0d799145574c25a37c89e63a5eeb2ca70710
Your Name <your_email@example.com>
2025-11-03 15:22:51 +0530

    Porta change

--- src/main/java/com/nurvi/FirstAzureCloudApp/controller/HelloController.java
+++ src/main/java/com/nurvi/FirstAzureCloudApp/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String sayHello(){
-        return "Hello from First Azure Cloud App!";
+        return "Hello from First Azure Cloud App!\nRe-dployed";
     }
 }
