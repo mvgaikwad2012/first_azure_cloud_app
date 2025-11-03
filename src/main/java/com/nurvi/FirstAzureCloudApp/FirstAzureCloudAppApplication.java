49d1e68e678b370a668b0df15eab345efdad80e0
Your Name <your_email@example.com>
2025-11-03 09:54:43 +0530

    first commit

---
+++ src/main/java/com/nurvi/FirstAzureCloudApp/FirstAzureCloudAppApplication.java
@@ -0,0 +1,13 @@
+package com.nurvi.FirstAzureCloudApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAzureCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAzureCloudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nurvi/FirstAzureCloudApp/FirstAzureCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurvi.FirstAzureCloudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAzureCloudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
